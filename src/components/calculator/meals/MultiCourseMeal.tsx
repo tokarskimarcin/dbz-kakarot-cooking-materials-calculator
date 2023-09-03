72153bd80e223ed52aea387f61a229a76383bb3d
Marcin <marcinxx1994@gmail.com>
2023-09-03 22:26:09 +0200

    Mutli-Course meal has own component

---
+++ src/components/calculator/meals/MultiCourseMeal.tsx
@@ -0,0 +1,17 @@
+import {Typography} from '@mui/material';
+import MealData from './data/Meal';
+
+
+export default function MultiCourseMeal(props: {data: MealData}) {
+    
+    return (
+      <>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {props.data.getType()}
+        </Typography>
+        <Typography variant="h5" component="div">
+            {props.data.name}
+        </Typography>
+      </>
+    );
+}
--- src/components/calculator/meals/data/MultiCourseMeal.ts
+++ src/components/calculator/meals/data/MultiCourseMeal.ts
@@ -1,5 +1,8 @@
-import Meal from "./Meal";
+import Meal from './Meal';
+import MultiCourseMealComponent from '../MultiCourseMeal';
 
 export default class MultiCourseMeal extends Meal {
     protected type: string = 'Multi-Course Meal';
+
+    getComponent = (): (props: {data: Meal}) => JSX.Element => MultiCourseMealComponent;
 }
