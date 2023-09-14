89667d768e4567d23277b6279d58e4cc2a0503bf
Marcin <marcinxx1994@gmail.com>
2023-09-14 23:20:46 +0200

    #3 added checkbox

--- src/components/calculator/calculated-materials/Material.tsx
+++ src/components/calculator/calculated-materials/Material.tsx
@@ -1,17 +1,22 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Checkbox, Typography } from "@mui/material";
 import {ExpandMore} from "@mui/icons-material";
 import MaterialData from "../meals/data/classes/Material";
+import * as React from 'react';
 
 export default function Material(props: {material: MaterialData, index: number, count: number}) {
     const numberFormatter = Intl.NumberFormat('en', {notation: 'compact'});
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation()
+    };
     return (
         <Accordion>
             <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls={`panel${props.index}-content`}
-            id={`panel${props.index}-header`}
+                expandIcon={<ExpandMore />}
+                aria-controls={`panel${props.index}-content`}
+                id={`panel${props.index}-header`}
             >
-                <Typography>{props.material.name} <b>x{numberFormatter.format(props.count)}</b></Typography>
+                <Checkbox sx={{p: '2px'}} onClick={handleClick}></Checkbox><Typography>{props.material.name} <b>x{numberFormatter.format(props.count)}</b></Typography>
             </AccordionSummary>
             <AccordionDetails>
                 <Typography>
